d56ca9786634cf19c655fec5ca8bd7fbfca41819
Grzegorz Kowalski <hvitr.ulfr@gmail.com>
2022-07-09 22:10:55 +0200

    Added driving licence to User in register form and domain

--- Vidly/Vidly/Controllers/MoviesController.cs
+++ Vidly/Vidly/Controllers/MoviesController.cs
@@ -93,6 +93,7 @@ namespace Vidly.Controllers
 
         [HttpPost]
         [ValidateAntiForgeryToken]
+        [Authorize(Roles = RoleName.CanManageMovies)]
         public ActionResult Save(Movie movie)
         {
             if (!ModelState.IsValid)
--- Vidly/Vidly/Models/AccountViewModels.cs
+++ Vidly/Vidly/Models/AccountViewModels.cs
@@ -65,6 +65,11 @@ namespace Vidly.Models
     public class RegisterViewModel
     {
         [Required]
+        [StringLength(255)]
+        [Display(Name = "Driving Licence")]
+        public string DrivingLicence { get; set; }
+
+        [Required]
         [EmailAddress]
         [Display(Name = "Email")]
         public string Email { get; set; }
